892f7a204b40ae0871dabb59d767d91a76ec8b98
akkii03 <akkiindoria72@gmail.com>
2022-09-10 22:52:37 +0530

    all done

--- src/index.js
+++ src/index.js
@@ -34,8 +34,62 @@ app.get('/totalActive',async(req,res)=>{
             _id:"total",
             "recovered":totalActive
         });
-})
+});
 
+app.get('/totalDeath',async(req,res)=>{
+    const user = await collection_Model.find();
+    let totalDeath = 0;
+    await user.map((item)=>{
+        totalDeath += item.death;
+        
+    });
+    
+    res.send(
+        {
+            _id:"total",
+            "death":totalDeath
+        });
+});
+
+app.get('/hotspotStates',async(req,res)=>{
+    const user = await collection_Model.find();
+    let infected = 0;
+    let recovered = 0;
+    let hotSpot = [];
+    await user.map((item)=>{
+        infected  = item.infected;
+        recovered = item.recovered;
+        let ans = (infected-recovered)/infected; 
+        ans = ans.toFixed(5);
+
+        if(ans>0.1) {
+            hotSpot = [...hotSpot,{state:item.state,rate:ans}];
+        } 
+    });
+
+    res.send(hotSpot);  
+});
+
+
+app.get('/healthyStates',async(req,res)=>{
+    const user = await collection_Model.find();
+    let mortallity = 0;
+    let death = 0;
+    let infected = 0;
+    let array = [];
+    await user.map((item)=>{
+        death  = item.death;
+        infected = item.infected;
+        let mortallity = death/infected; 
+        mortallity = mortallity.toFixed(5);
+
+        if(mortallity<0.005) {
+            array = [...array,{state:item.state,rate:mortallity}];
+        } 
+    });
+
+    res.send(array);  
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
